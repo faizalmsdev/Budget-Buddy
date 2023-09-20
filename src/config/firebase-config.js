13552072484350da3ee6700ccb3e6fe364e091c6
Dextrodark <faizalmohamed5302002@gmail.com>
2023-09-20 11:50:35 +0530

    Added Hooks

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth , GoogleAuthProvider } from "firebase/auth"
+import { getAuth , GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyB6wXv5hkRmpmW-8LxB2ugrSctd6bQXM7w",
@@ -18,3 +19,4 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
--- src/pages/expense-tracker/index.jsx
+++ src/pages/expense-tracker/index.jsx
@@ -1,5 +1,51 @@
+import { useState } from 'react';
+import { useAddTransaction } from '../../hooks/useAddTransaction'
 export const ExpenseTracker =()=>{
-    return <div>
-        Hello World Expense
-    </div>
+
+    const { addTransaction } = useAddTransaction();
+
+    const [description,setDescription] =useState("");
+    const [transactionAmount,setTransactionAmount] =useState(0);
+    const [transactionType,setTransactionType] =useState("expense");
+
+    const onSubmit = async (e)=>{
+        e.preventDefault();
+        addTransaction({description , transactionAmount , transactionType,});
+    }
+    return (
+    <>
+        <div className="expense-tracker">
+            <div className="container">
+                <h1>Budget Buddy</h1>
+                <div className="balance">
+                    <h3>Your Balance</h3>
+                    <h2> $0.00 </h2>
+                </div>
+                <div className="summary">
+                    <div className="income">
+                        <h4>Income</h4>
+                        <p>$0.00</p>
+                    </div>
+                    <div className="expenses">
+                        <h4>Expenses</h4>
+                        <p>$0.00</p>
+                    </div>
+                </div>
+                <form  className="add-transaction" onSubmit={onSubmit}>
+                    <input type="text" placeholder="Description" required onChange={(e)=> setDescription(e.target.value)} />
+                    <input type="number" placeholder="Amount" required onChange={(e)=> setTransactionAmount(e.target.value)}/>
+                    <input type="radio" required id="expense" checked={transactionType === "expense"} value="expense" onChange={(e)=> setTransactionType(e.target.value)}/>
+                    <label htmlFor='expense'>Expense</label>
+                    <input type="radio" id="income" value="income" checked={transactionType === "income"} onChange={(e)=> setTransactionType(e.target.value)} />
+                    <label htmlFor='income' >Income</label>
+
+                    <button type="submit">Add Transactions</button>
+                </form>
+            </div>
+        </div>
+        <div className="transactions">
+            <h3>Transactions</h3>
+        </div>
+    </>
+);
 }
